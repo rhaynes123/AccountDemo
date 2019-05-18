aaf2d18060dd603c7cb790dd9ac86d94d25ec10b
Richard <rahayn@yahoo.com>
2019-05-18 17:35:34 -0400

    Added account class

---
+++ CustomerAccountDemo/Accounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CustomerAccountDemo
+{
+    public class Accounts
+    {
+        public string Number { get; }
+        public string AccountHolder { get; set; }
+        public string AccountBalance { get;  }
+
+        public void DepositToAccount(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void WithdrawFromAccount(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
