10a2566c328f71e2cb3e937bab6be2d8a7abfb4d
Richard <rahayn@yahoo.com>
2019-05-19 12:07:47 -0400

    Modified functions and constructors

--- CustomerAccountDemo/Accounts.cs
+++ CustomerAccountDemo/Accounts.cs
@@ -1,28 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CustomerAccountDemo
 {
     public class Accounts
     {
+        private List<Transaction> allTransactions = new List<Transaction>();
+
         readonly Random num = new Random();
 
         public int Number { get; }
         public string AccountHolder { get; set; }
-        public decimal AccountBalance { get;  }
+
+        public decimal AccountBalance
+        {
+            get 
+            {
+                decimal balance = 0;
+
+                foreach(var newtransaction in allTransactions)
+                {
+                    balance += newtransaction.TransAmount;
+                }
+
+                return balance;
+            }
+              
+        }
 
         public void DepositToAccount(decimal amount, DateTime date, string note)
         {
+            if(amount <=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount),"Amount of deposit must be positive");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void WithdrawFromAccount(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
 
         public Accounts(string name, decimal currentbalance)
         {
-            Number = num.Next(100000, 999999);//Remeber this doesn't prevent duplicates
+            Number = num.Next(100000, 999999);//Remember this doesn't prevent duplicates
             AccountHolder = name;
-            AccountBalance = currentbalance;
+            DepositToAccount(currentbalance, DateTime.Now, "Current Balance");
+           
         }
     }
 }
